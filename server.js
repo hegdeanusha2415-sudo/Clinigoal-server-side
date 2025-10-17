7b6ea4a810573378de362049d10582c69c3ce441
hegdeanusha2415-sudo <hegdeanusha2415@gmail.com>
2025-10-17 16:15:35 +0530

    Fix duplicate PORT declaration for Render

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // ----------------- MIDDLEWARE -----------------
-app.use(cors());
+app.use(cors({
+  origin: ['https://clinigoal-client-side.vercel.app/', 'http://localhost:3000'],
+  credentials: true
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
